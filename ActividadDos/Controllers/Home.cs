7f0ed581ead7f4e42ff41a4e2338ab6f57a21558
julio19salazar <juliosalazar378@gmail.com>
2021-09-26 13:47:28 -0500

    Agregar archivos de proyecto.

---
+++ ActividadDos/Controllers/Home.cs
@@ -0,0 +1,20 @@
+﻿using ActividadDos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActividadDos.Controllers
+{
+    public class Home : Controller
+    {
+        public string Index()
+        {
+            return "hola para acceder a la vista de calcular promedio ingrese Home/PaginaPromedio";
+        } public IActionResult PaginaPromedio(PaginaPromedioViewModel vm)
+        {
+            return View(vm);
+        }
+    }
+}
---
+++ ActividadDos/Models/PaginaPromedioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActividadDos.Models
+{
+  public class PaginaPromedioViewModel
+    {
+        public double CalificacionUno { get; set; }
+        public double CalificacionDos { get; set; }
+        public double CalificacionTres { get; set; }
+        public double Resultado { get { return (CalificacionUno + CalificacionDos + CalificacionTres) / 3; } }
+        
+        public string Estado { get { return Resultado <= 100 && Resultado >= 70 ? "Aprobado" : "Reprobado"; } }
+    }
+}
